16e420c401eb6111b999254336a4bfa06d2fa74e
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-13 00:05:23 +0200

    add passege to message by user token

---
+++ UI/pages/messages.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="messages">
+      <div class="messages__leftBar">
+          <Label name="Rem" message="awdawdad" time="18:00" token="213"></Label>
+      </div>
+      <div class="messages__field">
+          <NuxtChild></NuxtChild>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Label from "../components/Messager/Label.vue";
+  export default {
+    layout: "default",
+    components: {
+      Label,
+    },
+  };
+  </script>
+  
+  <style lang="scss">
+  .messages {
+    width: 100%;
+    height: 800px;
+    background-color: #fff;
+    display: flex;
+    justify-content: start;
+    &__leftBar {
+      height: 100%;
+      width: 300px;
+      border-right: 1px solid rgba(88, 88, 88, 0.389);
+    }
+  }
+  </style>
---
+++ UI/pages/messages/_id.vue
@@ -0,0 +1,14 @@
+<template>
+
+<div>{{id}}</div>
+
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const id = params.id; // When calling /abc the slug will be "abc"
+    return { id };
+  },
+};
+</script>
