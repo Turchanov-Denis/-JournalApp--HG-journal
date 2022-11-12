5f7b08f3d884121797de88a1ac624489f8f884fe
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-13 00:54:35 +0200

    add dependency for notification from store

--- UI/pages/notification.vue
+++ UI/pages/notification.vue
@@ -1,24 +1,28 @@
 <template>
-    <div class="notifications">
-        
+  <div class="notifications">
+    <div
+      v-for="(item, index) in this.$store.state.notification.notification"
+      :key="index"
+    >
+      <div style="color: black; text-align: center; padding: 20px">
+        {{ item }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default{
-    layout: 'default'
-}
+export default {
+  layout: "default",
+};
 </script>
 
 <style lang="scss">
-
-
 .notifications {
-    width: 60%;
-    margin-top: 30px;
-    height: 400px;
-    background-color: #fff;
-    border-radius: 10px;
-    
+  width: 60%;
+  margin-top: 30px;
+  height: 400px;
+  background-color: #fff;
+  border-radius: 10px;
 }
 </style>
---
+++ UI/store/notification.js
@@ -0,0 +1,3 @@
+export const state = () => ({
+    notification: ['Ra', 'Fu']
+})
