3298907b68257bdd7a2c29c511903eba0df50ad8
Turchanov Denis <turchanovdenis786@gmail.com>
2022-11-13 16:12:37 +0200

    add database: postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env'
+  }),SequelizeModule.forRoot({
+    dialect: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    models: [],
+    autoLoadModels: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 
